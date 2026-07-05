9fd3809ca7803c5f7c521958f1e012198cd67317
ADONYARIK <scityrchenko@gmail.com>
2026-07-06 00:24:37 +0200

    Creating base blade template, controller and route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CvController;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('cv')->group(function(){
+    Route::controller(CvController::class)->group(function(){
+        Route::get('/', 'index');
+    });
 });
